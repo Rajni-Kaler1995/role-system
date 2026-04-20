f1cb514c1035d1614153846e59c4cba11797d63c
rajnicybernauticstech <cybernauticstech.development@gmail.com>
2026-04-20 22:33:26 +0530

    done some changes

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     public function users()
-{
-    return $this->hasMany(User::class);
-}
+    {
+        return $this->hasMany(User::class);
+    }
 
 }
